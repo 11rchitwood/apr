74d84e7e27f483fcd6fbdc6019ad86d58751c651
Ryan Chitwood <chitwoodrw@icloud.com>
2025-10-24 12:50:21 -0400

    Add apr_rotate

--- R/apr.R
+++ R/apr.R
@@ -17,3 +17,22 @@ apr_reshape <- function(x, value) {
   dim(x) <- value
   x
 }
+
+#' Rotate a vector
+#'
+#' @param x a vector
+#' @param k an integer, the position to rotate
+#'
+#' @return a vector
+#'
+#' @examples
+#' rotate(1:12, 3)
+#'
+#' @export
+apr_rotate <- function(x, k) {
+  n <- length(x)
+  x <- rev(x)
+  x[1:k] <- rev(x[1:k])
+  x[(k + 1):n] <- rev(x[(k + 1):n])
+  x
+}
--- tests/testthat/test-apr.R
+++ tests/testthat/test-apr.R
@@ -3,4 +3,5 @@ test_that("everything works", {
   arr <- array(x, c(3, 2, 2))
   expect_equal(apr_shape(arr), c(3, 2, 2))
   expect_equal(apr_reshape(arr, c(2, 3, 2)), array(x, dim = c(2L, 3L, 2L)))
+  expect_equal(apr_rotate(x, 3), c(10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9))
 })
